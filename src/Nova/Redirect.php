4862a2643ccc1942b3b63194373333e48066b0f7
Stef van Esch <stef@marshmallow.dev>
2021-07-01 19:34:46 +0200

    Icon update

--- src/Nova/Redirect.php
+++ src/Nova/Redirect.php
@@ -19,6 +19,13 @@ class Redirect extends Resource
      */
     public static $model = 'Marshmallow\Redirectable\Models\Redirect';
 
+    public static $group_icon = '<svg xmlns="http://www.w3.org/2000/svg" class="sidebar-icon" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path fill="var(--sidebar-icon)" d="M3 2v12h3v9l7-12H9l4-9H3zm16 0h-2l-3.2 9h1.9l.7-2h3.2l.7 2h1.9L19 2zm-2.15 5.65L18 4l1.15 3.65h-2.3z"/></svg>';
+
+    public static function group()
+    {
+        return __('SEO');
+    }
+
     /**
      * The single value that should be used to represent the resource when being displayed.
      *
